b696d80234447f63632a3991f318da5e0e53fd31
Chris Eldridge <cre@mojotech.com>
2016-10-17 16:31:24 -0400

    create api request

--- Fetch/ViewController.swift
+++ Fetch/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
+import Alamofire
+
 class ViewController: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        Alamofire.request("https://www.mojotech.com/people.json").responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
