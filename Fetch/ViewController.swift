831ef309d7ccb9abf061bfe4f40d9b3f35677621
Chris Eldridge <cre@mojotech.com>
2016-10-17 15:58:52 -0400

    create new project

---
+++ Fetch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fetch
+//
+//  Created by Chris Eldridge on 10/17/16.
+//  Copyright © 2016 Chris Eldridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
